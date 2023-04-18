8d5847a31e4b7d08954f536d01b4610244f9f801
dariasv1108 <dariasv01@gmail.com>
2023-04-18 21:00:40 +0200

    ObtenerModulosMariaDB

--- main.py
+++ main.py
@@ -8,37 +8,52 @@ app = FastAPI()
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
-    user="user",
+    user="nombre",
     password="pass",
-    database="nameDB"
+    database="nombreDB"
 )
+
+data = {"id": 1, "nombre": "John Doe", "activo": True}
+
+
 @app.get("/")
 async def root():
-    return {"temperatura": str(random.randint(1, 50)),"ruido":str(random.randint(1, 90)),"bares":str(random.randint(900, 1500))}
+    return {"temperatura": str(random.randint(1, 50)), "ruido": str(random.randint(1, 90)),
+            "bares": str(random.randint(900, 1500))}
+
 
 @app.get("/obtenerModulos")
 async def obtenerModulos():
     mycursor = mydb.cursor()
+    query = "SELECT * FROM modulo WHERE activo=True"
+    mycursor.execute(query)
+    results = []
+    for row in mycursor.fetchall():
+        results.append({
+            'id': row[0],
+            'nombre': row[1],
+            'activo': row[2]
+        })
+    mycursor.close()
 
-    mycursor.execute("SELECT * FROM modulo")
-
-    myresult = mycursor.fetchall()
-    return {"result": myresult}
+    return results
 
 @app.get("/obtenerDatosGenerales")
 async def obtenerDatosGenerales():
-    return {"temperatura": str(random.randint(1, 50)),"ruido":str(random.randint(1, 90)),"bares":str(random.randint(900, 1500))}
+    return {"temperatura": str(random.randint(1, 50)), "ruido": str(random.randint(1, 90)),
+            "bares": str(random.randint(900, 1500))}
 
 
 @app.get("/obtenerDatosModulo/{id}")
 async def obtenerDatosModulo(id: str):
-    return {"temperatura": str(random.randint(1, 50)),"ruido":str(random.randint(1, 90)),"bares":str(random.randint(900, 1500))}
+    return {"temperatura": str(random.randint(1, 50)), "ruido": str(random.randint(1, 90)),
+            "bares": str(random.randint(900, 1500))}
 
 
 if __name__ == '__main__':
     import uvicorn
 
-    origins = ['http://localhost:8888']
+    origins = ['http://localhost:8888', 'http://informatica.iesalbarregas.com:8888']
 
     app.add_middleware(
         CORSMiddleware,
