767df570d71e3166ff5e2a734a26987407ddf518
dariasv1108 <dariasv01@gmail.com>
2023-04-18 19:55:09 +0200

    conexion mariadb

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 import random
+import mysql.connector
 
 from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="user",
+    password="pass",
+    database="nameDB"
+)
 @app.get("/")
 async def root():
     return {"temperatura": str(random.randint(1, 50)),"ruido":str(random.randint(1, 90)),"bares":str(random.randint(900, 1500))}
+
+@app.get("/obtenerModulos")
+async def obtenerModulos():
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM modulo")
+
+    myresult = mycursor.fetchall()
+    return {"result": myresult}
 
 @app.get("/obtenerDatosGenerales")
 async def obtenerDatosGenerales():
@@ -22,7 +38,7 @@ async def obtenerDatosModulo(id: str):
 if __name__ == '__main__':
     import uvicorn
 
-    origins = ['http://localhost:8000', 'TuURL']
+    origins = ['http://localhost:8888']
 
     app.add_middleware(
         CORSMiddleware,
@@ -31,4 +47,4 @@ if __name__ == '__main__':
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8888)
